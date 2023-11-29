57f23e7bc3abcc4053f686da3c4c9d5b0b4598f9
Ivan <122544806+SomExcell@users.noreply.github.com>
2023-11-29 12:20:18 +0500

    Add files via upload

---
+++ String/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+
+#include "String.h"
+
+int main()
+{
+	String x = "avs";
+	String a = x + x;
+	std::cout << a;
+	return 0;
+}
